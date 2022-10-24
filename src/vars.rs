c15ebad0ba1d0a88e78f259f7c9ee568ad7789bd
TAK848 <pairs_char0j@icloud.com>
2022-10-25 08:33:49 +0900

    str

--- src/vars.rs
+++ src/vars.rs
@@ -49,4 +49,30 @@ pub fn run() {
     let a1 = [1, 2, 3, 4, 5];
     let a2 = [0; 10];
     println!("{:?} {:?} {} {}", a1, a2, a1[2], a1[3]);
+
+    let s1 = "hello, こんにちは，世界";
+    let s2 = "hello";
+    println!("Stack address of s1 is: {:p}", &s1);
+    println!("Stack address of s2 is: {:p}", &s2);
+    println!("Static memory address of s1 is: {:?}", s1.as_ptr());
+    println!("Static memory address of s2 is: {:?}", s2.as_ptr());
+    println!("Len of s1 is: {}", s1.len());
+    println!("Len of s2 is: {}", s2.len());
+    let mut s1 = String::from("Hello");
+    let mut s2 = String::from("Hello");
+    println!("Stack address of s1 is: {:p}", &s1);
+    println!("Stack address of s2 is: {:p}", &s2);
+    println!("Heap memory address of s1: {:?}", s1.as_ptr());
+    println!("Heap memory address of s2: {:?}", s2.as_ptr());
+    println!("Len of s1 is: {}", s1.len());
+    println!("Len of s2 is: {}", s2.len());
+    println!("Capacity of s1 is: {}", s1.capacity());
+    println!("Capacity of s2 is: {}", s2.capacity());
+    s1.push_str("_new1");
+    s2.push_str("_new2!");
+    println!("{} {}", s1, s2);
+    println!("Len of s1 is: {}", s1.len());
+    println!("Len of s2 is: {}", s2.len());
+    println!("Capacity of s1 is: {}", s1.capacity());
+    println!("Capacity of s2 is: {}", s2.capacity());
 }
