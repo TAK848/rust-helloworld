06564027346020ff7f548c6839ba2199bcd2ad47
TAK848 <pairs_char0j@icloud.com>
2022-10-26 14:30:45 +0900

    struct

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 // mod vars;
 // mod ownership;
 // mod generics;
-mod lifetime;
+// mod lifetime;
+mod structs;
 fn main() {
     // println!("Hello, world!");
     // vars::run();
     // stack_heap::run();
     // ownership::run();
     // generics::run();
-    lifetime::run();
+    // lifetime::run();
+    structs::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
