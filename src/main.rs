e742cc4b9f704a80b118db7247d6a08d5e326e5c
TAK848 <pairs_char0j@icloud.com>
2022-10-26 13:31:46 +0900

    ownership 言うほど難しくはない

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-mod stack_heap;
-mod vars;
+// mod stack_heap;
+// mod vars;
+mod ownership;
 fn main() {
     // println!("Hello, world!");
     // vars::run();
-    stack_heap::run();
+    // stack_heap::run();
+    ownership::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
